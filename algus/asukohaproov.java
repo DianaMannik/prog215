e95f4281697190177660e1d7e9c6cf051be5e538
Diana Männik <diana.mannik@tigu.hk.tlu.ee>
2015-09-03 12:02:22 +0300

    asukoht

---
+++ algus/asukohaproov.java
@@ -0,0 +1,7 @@
+public class asukohaproov{
+   public static void main(String[] arg){
+      asukoht haapsalu=new asukoht(58.936864, 23.540753);
+	  asukoht tartu=new asukoht(58.371829, 26.729506);
+      System.out.println(haapsalu+" "+tartu);
+   }
+}
---
+++ algus/asukoht.java
@@ -0,0 +1,14 @@
+class asukoht{
+   public asukoht(double laiuskraad, double pikkuskraad){
+      lat=kraadParajaks(laiuskraad);
+	  lng=kraadParajaks(pikkuskraad);
+   }
+   private double kraadParajaks(double kraad){
+      while(kraad>180) {kraad-=360;}
+      while(kraad<-180){kraad+=360;}
+	  return kraad;
+   }
+   public @Override String toString(){ return "("+lat+","+lng+")";}
+   double lat;
+   double lng;
+}
