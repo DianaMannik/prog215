99ba0a45c219022e3c4a570596707078511ebbcd
Diana Männik <diana.mannik@tigu.hk.tlu.ee>
2015-09-03 12:35:11 +0300

    asi

--- algus/asukohaproov.java
+++ algus/asukohaproov.java
@@ -4,5 +4,13 @@ public class asukohaproov{
 	  asukoht tartu=new asukoht(58.371829, 26.729506);
       System.out.println(haapsalu+" "+tartu);
 	  System.out.println(haapsalu.getLat());
+	  System.out.println(tartu.kaugus(haapsalu));
+	  asukoht[] teekond=new asukoht[3];
+	  teekond[0]=haapsalu;
+	  teekond[1]=new asukoht(58.887944, 25.568624);
+	  teekond[2]=tartu;
+	  for(int i=0; i<teekond.length; i++){
+	    System.out.println(teekond[i]);
+	  }
    }
 }
