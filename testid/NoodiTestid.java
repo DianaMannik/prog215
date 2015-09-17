eba141c44d7aad6cdc24d7bdbc852596c1fb1bb6
Diana Männik <diana.mannik@tigu.hk.tlu.ee>
2015-09-17 13:03:13 +0300

    noot

--- testid/NoodiTestid.java
+++ testid/NoodiTestid.java
@@ -4,12 +4,17 @@ public class NoodiTestid{
 	@Test
 	public void loomine(){
 		Noot n1=new Noot("A");
+		try{
+		  Noot n2=new Noot("X");
+		  fail("Loodi lubamatu noot");
+		} catch(Exception ex){}
 	}
 	@Test
 	public void korgus(){
 		Noot n1=new Noot("A");
 		assertEquals(69, n1.kysiKorgus());
-		Noot n1=new Noot("c");
+		//Lisage C kõrgusega 60
+		Noot n2=new Noot("C");
 		assertEquals(60, n2.kysiKorgus());
 	}
 }
